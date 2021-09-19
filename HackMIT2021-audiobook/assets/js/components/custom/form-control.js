12ebc28468fec12d3c32f8db5f5b8f54e72274bb
ang-li7 <li.angela.0220@gmail.com>
2021-09-18 20:08:07 -0400

    Text input, audio input

--- HackMIT2021-audiobook/assets/js/components/custom/form-control.js
+++ HackMIT2021-audiobook/assets/js/components/custom/form-control.js
@@ -19,7 +19,13 @@ var FormControl = (function() {
     }).trigger('blur');
 	}
 
-
+	function handleFiles(event) {
+		var files = event.target.files;
+		$("#src").attr("src", URL.createObjectURL(files[0]));
+		document.getElementById("audio").load();
+	}
+	
+	document.getElementById("upload").addEventListener("change", handleFiles, false);
 	// Events
 
 	if ($input.length) {
